ea6e7a58171ab924d13da39db70908b6408b78f0
SealCandy <pavlikholecek2@email.cz>
2022-11-11 12:22:57 +0100

    Creation of .EF project and the removal of the previous one

---
+++ ProjectWeb/ProjectWeb/Model/Enum/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectWeb.Model.Enum
+{
+    public enum Gender
+    {
+        MALE = 0,
+        FEMALE = 1,
+        OTHER = 2,
+    }
+}
---
+++ ProjectWeb/ProjectWeb/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ProjectWeb.Model.Enum;
+
+namespace ProjectWeb.Model
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Forename { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Password { get; set; }
+        
+        public Gender Gender  { get; set; }
+    }
+}
